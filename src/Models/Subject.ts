6915aac184a2bb69b8ff105a583c22c048f71e3d
Joey Lim <joeylim@Joeys-MacBook-Air.local>
2022-02-23 20:27:49 +0800

    added Subject.ts model for the Subject data

---
+++ src/Models/Subject.ts
@@ -0,0 +1,10 @@
+export interface Subject {
+    name: String,
+    abbreviation: String,
+    description: String,
+    color: String,
+    image: String,
+    resources: Array<String>
+    advanced: Array<String>,
+    software: Array<String>
+}
