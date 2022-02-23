f2eba5b08dcbbebe6fa50f51862b0c0800edaf3a
Putt <57494734+PuttTim@users.noreply.github.com>
2022-02-24 06:30:09 +0800

    About page content added

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -54,6 +54,45 @@ const About = () => {
 					<Image src="../../assets/itsig-icon.svg" w="60%" h="60%" />
 				</HStack>
 			</Box>
+			<VStack
+				spacing={4}
+				display={{ base: "flex" }}
+				align="left"
+				justify="left"
+				pt={{ base: "24" }}
+				px={{ base: "4", lg: "64" }}
+				pb={24}>
+				<Text
+					fontSize={{ base: "1.5em", lg: "2.5em" }}
+					fontWeight="bold">
+					What is this website about?
+				</Text>
+				<Text fontSize={{ base: "1em", lg: "1.25em" }}>
+					We know the transition from secondary school or ITE to Poly
+					can come as a big shock to you in terms of what we learn, so
+					we made this website for IIT freshmen to provide resources
+					for you to view in your free time before school starts so
+					that you have a clearer idea of what to expect in your first
+					semester with us.
+				</Text>
+
+				<Text
+					textAlign="left"
+					fontSize={{ base: "1.5em", lg: "2.5em" }}
+					fontWeight="bold">
+					Who are we?
+				</Text>
+				<Text fontSize={{ base: "1em", lg: "1.25em" }} pt={-4}>
+					We are a CCA consisting of students from the Diploma in
+					Information Technology. We plan events that improve the
+					student life in our diploma. Such events include technical
+					workshops to introduce new technology, coming up with
+					diploma windbreakers, starting software projects like this
+					website, and many more. Follow us on Instagram for more
+					updates and we hope you learn a thing or two from our
+					events! :)
+				</Text>
+			</VStack>
 		</>
 	)
 }
