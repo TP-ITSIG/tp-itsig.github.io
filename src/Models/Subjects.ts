cd985fe2d0c856a672b524427ec84220db147a18
Joey Lim <joeylim@Joeys-MacBook-Air.local>
2022-02-23 22:06:10 +0800

    added more dummy data

--- src/Models/Subjects.ts
+++ src/Models/Subjects.ts
@@ -3,6 +3,7 @@ import { ResourceType } from './ResourceType'
 export let subjects: Subject[] = [
     {
         name: "Computational Thinking",
+        tagline: "Develop an Android Application",
         abbreviation: "COMT",
         description: "This module teaches the fundamentals of programming using the Java programming language. Using your new skills, you will transform your UXID prototype into a real Android app with Android Studio.",
         color: "hsl(154, 74%, 69%)",
@@ -11,7 +12,8 @@ export let subjects: Subject[] = [
             title:  "Learning How to Code", 
             content: "A fun video to introduce to you how you should approach learning how to code. It also introduces different programming languages and their uses.", 
             thumbnail: "https://www.youtube.com/watch?v=Z9kg24yad-0&list=PLkdNeERjj7-6n0ItDJZQMD6561UzxDQ_B&index=1", 
-            type: ResourceType.VIDEO },
+            type: ResourceType.VIDEO 
+            },
             {
                 title: "Android Studio Starter",
                 content: "An explanation of what an Integrated Development Environment (IDE) is and what Android Studio is about.",
@@ -28,5 +30,158 @@ export let subjects: Subject[] = [
         ],
         advanced: [""],
         software: [""]
-    }
+    },
+    {
+        name: "Data Analytics and Visualisation",
+        tagline: "Visualise and analyse big data",
+        abbreviation: "DAVA",
+        description: "",
+        color: "hsl(154, 74%, 69%)",
+        image: "./assets/DAVA.svg",
+        resources: [{
+            title:  "", 
+            content: "", 
+            thumbnail: "", 
+            type: ResourceType.VIDEO 
+            },
+            {
+                title: "",
+                content: "",
+                thumbnail: "",
+                type: ResourceType.VIDEO
+            },
+            {
+                title: "",
+                content: "",
+                thumbnail: "",
+                type: ResourceType.VIDEO
+            },
+        
+        ],
+        advanced: [""],
+        software: [""]
+    },
+    {
+        name: "Network Technology",
+        tagline: "Learn about the internet and simulate computer networks",
+        abbreviation: "NETY",
+        description: "",
+        color: "hsl(154, 74%, 69%)",
+        image: "./assets/NETY.svg",
+        resources: [{
+            title:  "", 
+            content: "", 
+            thumbnail: "", 
+            type: ResourceType.VIDEO 
+            },
+            {
+                title: "",
+                content: "",
+                thumbnail: "",
+                type: ResourceType.VIDEO
+            },
+            {
+                title: "",
+                content: "",
+                thumbnail: "",
+                type: ResourceType.VIDEO
+            },
+        
+        ],
+        advanced: [""],
+        software: [""]
+    },
+    {
+        name: "User Experience and Interface Design",
+        tagline: "Design and prototype your app",
+        abbreviation: "UXID",
+        description: "",
+        color: "hsl(154, 74%, 69%)",
+        image: "./assets/UXID.svg",
+        resources: [{
+            title:  "", 
+            content: "", 
+            thumbnail: "", 
+            type: ResourceType.VIDEO 
+            },
+            {
+                title: "",
+                content: "",
+                thumbnail: "",
+                type: ResourceType.VIDEO
+            },
+            {
+                title: "",
+                content: "",
+                thumbnail: "",
+                type: ResourceType.VIDEO
+            },
+        
+        ],
+        advanced: [""],
+        software: [""]
+    },
+    {
+        name: "Logic and Mathematics",
+        tagline: "Learn about Discrete Mathematics",
+        abbreviation: "LOMA",
+        description: "",
+        color: "hsl(154, 74%, 69%)",
+        image: "./assets/LOMA.svg",
+        resources: [{
+            title:  "", 
+            content: "", 
+            thumbnail: "", 
+            type: ResourceType.VIDEO 
+            },
+            {
+                title: "",
+                content: "",
+                thumbnail: "",
+                type: ResourceType.VIDEO
+            },
+            {
+                title: "",
+                content: "",
+                thumbnail: "",
+                type: ResourceType.VIDEO
+            },
+        
+        ],
+        advanced: [""],
+        software: [""]
+    },
+    {
+        name: "Others",
+        tagline: "Usefil miscellaneous resources",
+        abbreviation: "Others",
+        description: "",
+        color: "hsl(154, 74%, 69%)",
+        image: "./assets/Others.svg",
+        resources: [{
+            title:  "", 
+            content: "", 
+            thumbnail: "", 
+            type: ResourceType.VIDEO 
+            },
+            {
+                title: "",
+                content: "",
+                thumbnail: "",
+                type: ResourceType.VIDEO
+            },
+            {
+                title: "",
+                content: "",
+                thumbnail: "",
+                type: ResourceType.VIDEO
+            },
+        
+        ],
+        advanced: [""],
+        software: [""]
+    },
+
+
+
 ];
