5cc99d225f075cde001ce266d53a9870a5d39c05
Joey Lim <joeylim@Joeys-MacBook-Air.local>
2022-02-23 21:22:32 +0800

    added a type field using ResourceType enum

--- src/Models/Resource.ts
+++ src/Models/Resource.ts
@@ -1,5 +1,7 @@
+import { ResourceType } from './ResourceType'
 export interface Resource {
     title: String,
     content: String,
-    thumbnail: String
+    thumbnail: String,
+    type: ResourceType
 }
