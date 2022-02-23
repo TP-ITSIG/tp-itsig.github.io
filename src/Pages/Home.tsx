f1fecdcb6f46d058f0c70f3f931ba1ae6cbe6709
Putt <57494734+PuttTim@users.noreply.github.com>
2022-02-24 00:26:14 +0800

    Refactored Home page's header to use em's and not px. Thanks @TechSupportz

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,15 +1,5 @@
 import React from "react"
-import {
-	Box,
-	Center,
-	Tooltip,
-	IconButton,
-	VStack,
-	HStack,
-	Text,
-	Icon,
-	Image,
-} from "@chakra-ui/react"
+import { Box, VStack, HStack, Text, Image } from "@chakra-ui/react"
 
 const Home = () => {
 	return (
@@ -19,22 +9,21 @@ const Home = () => {
 				alignItems="center"
 				justifyContent="center"
 				w="100%"
-				pt={{ base: "16px", md: "60px" }}>
+				pt={{ base: "4", md: "16" }}>
 				<VStack spacing={0}>
 					<Text
-						fontSize={{ base: "24px", md: "40px" }}
+						fontSize={{ base: "1.5em", md: "2.5em" }}
 						fontWeight="bold">
 						Welcome to the
 					</Text>
 					<Text
-						fontSize={{ base: "24px", md: "40px" }}
+						fontSize={{ base: "1.5em", md: "2.5em" }}
 						fontWeight="bold"
 						color="iit_blue">
 						School of Informatics & IT
 					</Text>
-
 					<Text
-						fontSize={{ base: "12px", md: "20px" }}
+						fontSize={{ base: "0.75em", md: "1.25em" }}
 						fontWeight="semibold"
 						pt={2}
 						px={{ base: "8", md: "24" }}>
@@ -63,17 +52,18 @@ const Home = () => {
 						display="flex"
 						alignItems="left"
 						justifyContent="left">
-						<Text fontSize={{ base: "50px" }} fontWeight="bold">
+						<Text fontSize={{ base: "3.125em" }} fontWeight="bold">
 							Welcome to the
 						</Text>
 						<Text
-							fontSize={{ base: "50px" }}
+							fontSize={{ base: "3.125em" }}
 							fontWeight="bold"
 							color="iit_blue">
 							School of Informatics & IT
 						</Text>
-
-						<Text fontSize={{ base: "12px", md: "20px" }} pt={8}>
+						<Text
+							fontSize={{ base: "0.75em", md: "1.25em" }}
+							pt={8}>
 							This website provides resources to give you a
 							general overview of what to expect for each module
 							so that you might feel better prepared for your
