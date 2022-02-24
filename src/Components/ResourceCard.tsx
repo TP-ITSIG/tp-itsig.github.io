7e7d6ce63e1196f66b87095f9eb4fee4c81d2a9e
Joey Lim <joeylim@Joeys-MacBook-Air.local>
2022-02-24 14:17:06 +0800

    modified resource card

--- src/Components/ResourceCard.tsx
+++ src/Components/ResourceCard.tsx
@@ -2,14 +2,16 @@ import {
     Flex,
 	Box,
     SimpleGrid,
-    AspectRatio
+    AspectRatio,
+    Spacer,
+    Image
 } from "@chakra-ui/react"
 import { Resource } from '../Models/Resource'
 import { ResourceType } from "../Models/ResourceType"
 const ResourceCard: React.FC<{resource: Resource, reverse: boolean}> = ({resource, reverse}) => {
     return (
-        <Flex bgColor={"beige"} marginBottom={10} direction={{base: 'column', md: reverse ? 'row-reverse' : 'row'}}>
-            <Box flex={2}>
+        <Flex marginBottom={10} direction={{base: 'column', md: reverse ? 'row-reverse' : 'row'}}>
+            <Box flex={3}>
                 { resource.type == ResourceType.VIDEO ? (
                     <AspectRatio>
                         <iframe width={"100%"} height={"100%"} src={resource.thumbnail} allowFullScreen ></iframe>
@@ -17,10 +19,11 @@ const ResourceCard: React.FC<{resource: Resource, reverse: boolean}> = ({resourc
                     </AspectRatio>
 
                 )
-                    : <img width={"100%"} height={"100%"} src={resource.thumbnail}/>
+                    : <Image width={"100%"} height={"100%"} src={resource.thumbnail}/>
                 }
             </Box>
-            <Box flex={3}>
+            <Spacer />
+            <Box flex={5}>
                 <SimpleGrid columns={1}>
                     <Box fontWeight={800} fontSize={{base: "24", md: "36", lg:"48"}}>{resource.title}</Box>
                     <Box fontSize={{base: "14", md:"16", lg:"24"}}>{resource.content}</Box>
