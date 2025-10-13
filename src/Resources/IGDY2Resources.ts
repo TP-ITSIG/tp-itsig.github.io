5ea97463bd5079954123af35a663b30553068e0a
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-10-13 08:22:09 +0800

    Update GADV

--- src/Resources/IGDY2Resources.ts
+++ src/Resources/IGDY2Resources.ts
@@ -2,25 +2,51 @@ import { ResourceType } from "../Interfaces/ResourceType"
 import { Subject } from "../Interfaces/Subject"
 
 const GADV: Subject = {
-    name: "",
+    name: "Game Development",
     tagline: "",
-    abbreviation: "",
-    description: "",
-    color: "",
-    bgColor: "",
+    abbreviation: "GADV",
+    description: "This subject provides you with the knowledge and skills to develop graphical interactive games through the use of existing game libraries and to create the component parts of a game, both assets and programming code, and then bring them together to produce a complete game. The subject covers game development techniques such as sprite creation, rendering and animation; collision detection; the main game loop; event handling and control of the frame rate. The in-game usage of sound effects will also be taught, as well as key programming concepts required in game development such as memory management, programming standards and debugging.",
+    color: "hsl(226, 67%, 67%)",
+	bgColor: "hsla(226, 67%, 67%, 0.3)",
     imageLight: "",
     imageDark: "",
     resources: [
         {
-            title: "",
-            content: "",
+            title: "C# in 100 seconds",
+            content: "Understand the language behind Unity, and how C# works!",
             thumbnail: "",
             type: ResourceType.IMAGE,
-            imageUrl: "",
+            imageUrl: "https://youtu.be/ravLFzIguCM?si=HJOW2HyHRw3kGhil",
             resourceLinks: [
                 {
-                    title: "",
-                    url: ""
+                    title: "You can watch the video here",
+                    url: "https://youtu.be/ravLFzIguCM?si=HJOW2HyHRw3kGhil"
+                }
+            ]
+        },
+        {
+            title: "Unity in 100 seconds",
+            content: "Understand how the Unity game engine works and learn how to get started on your game development project!",
+            thumbnail: "",
+            type: ResourceType.IMAGE,
+            imageUrl: "https://youtu.be/iqlH4okiQqg?si=FgjXvLmayzN0WM22",
+            resourceLinks: [
+                {
+                    title: "You can watch the video here",
+                    url: "https://youtu.be/iqlH4okiQqg?si=FgjXvLmayzN0WM22"
+                }
+            ]
+        },
+        {
+            title: "Make a simple game!",
+            content: "If you are interested to learn how to use C# to develop a game, you can follow these videos and try it out!",
+            thumbnail: "",
+            type: ResourceType.IMAGE,
+            imageUrl: "https://youtube.com/playlist?list=PLcRSafycjWFeHojgzbkNUXt9S6-Q7BGQE&si=D12asg7dHCWYNE_I",
+            resourceLinks: [
+                {
+                    title: "Follow this playlist!",
+                    url: "https://youtube.com/playlist?list=PLcRSafycjWFeHojgzbkNUXt9S6-Q7BGQE&si=D12asg7dHCWYNE_I"
                 }
             ]
         }
